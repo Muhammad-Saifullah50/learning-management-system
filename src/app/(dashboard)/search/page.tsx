3d5f6c2f8e586157d4da95d671acf3ada5856f4f
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-01-19 22:30:38 +0500

    completed search

--- src/app/(dashboard)/search/page.tsx
+++ src/app/(dashboard)/search/page.tsx
@@ -1,16 +1,21 @@
 import { getCategories } from "@/actions/category.action"
 import Categories from "@/components/Categories";
+import SearchInput from "@/components/SearchInput";
 
 const SearchPage = async () => {
 
   const categories = await getCategories();
   return (
-    <div className="p-6">
-      <Categories
-        items={categories}
-      />
-    </div>
-  )
+    <>
+      <div className="p-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories
+          items={categories}
+        />
+      </div>
+    </>)
 }
 
 export default SearchPage
