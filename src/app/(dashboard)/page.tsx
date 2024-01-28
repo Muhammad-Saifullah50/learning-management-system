2f9dcc772ec9d207e1700109ea8039b6b324e219
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-01-28 10:56:05 +0500

    corrected metadata

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: 'Home',
+  title: 'Home - Learnitees',
 }
 export default async function Dashboard() {
 
